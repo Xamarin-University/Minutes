8fc4c9d66a29e995f3e17ddeca90ef40ce263ccb
Pedro Barriga <petter_black05@hotmail.com>
2018-05-10 13:26:51 -0700

    commit inicial

---
+++ Minutes/Minutes/Entry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minutes
+{
+    public class Entry
+    {
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public override string ToString()
+        {
+            return Title + " " + CreatedDate;
+        }
+
+
+    }
+}
---
+++ Minutes/Minutes/IEntryStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minutes
+{
+    public interface IEntryStore
+    {
+
+        Task<List<Entry>> ReadAsync();
+        Task WriteAsync(Entry entry);
+        Task DeleteAsync(Entry entry);
+
+    }
+}
